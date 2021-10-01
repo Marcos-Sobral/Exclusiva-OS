5c32fe772216ddde954ac6fc1a58aa09a1c2347d
Marcos Sobral <vini15oficial@gmail.com>
2021-10-01 14:12:30 -0300

    Erro Migrate 2002

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
+
+Route::get('/user', [UserController::class, 'get']);
+Route::get('/cadastro', [UserController::class, 'get']);
+//Route::redirect('/usuario','/', 301);
+
+/*
+route::($url, $callback); // trazer registro 
+route::post($url, $callback); // inserção de registro
+route::put($url, $callback); // edição de registro 
+route::patch ($url, $callback); // edição de registro
+route::delete($url, $callback); //exclusão de registro, é obrigatório ter o tooker CRF 
+
+                 rota chamada | para onde ela vai | sei lá 
+Route::redirect(     ' /  ',    'usuario',           301); //redirecionamento de rotas
+
+
+Parametro // sub abas como exemplo Masculino -> Adulto -> Camisa -> social -> manga longa -> estampada 
+
+Route::get('usuario/{cadastro}', function ($cadastrp) {
+    return view 'welcome'. $cadastro;
+});
+
+*/
